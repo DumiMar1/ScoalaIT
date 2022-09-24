dbae5978b7d75b17af8b043b6faf93e7cf3350bd
Dumitru Marin <marindidi96@gmail.com>
2022-09-24 13:58:47 +0300

    Initial commit

---
+++ Exercitii/Tema 3/def_functions.py
@@ -0,0 +1,30 @@
+def palidrom(num):
+    if str(num) == str(num[::-1]):
+        return True
+    else:
+        return False
+
+def is_prime(num):
+    num = int(num)
+    if num > 1:
+        for i in range(2, num):
+            if num % i == 0:
+                return False
+        return True
+
+def divisor(num):
+    divisors_list = []
+    num = int(num)
+    for i in range(2, num):
+        if num % i == 0:
+            divisors_list.append(i)
+    return divisors_list
+
+def max_divisor(num):
+
+    return max(divisor(num))
+
+
+def digits(num):
+    return len(num)
+
---
+++ Exercitii/Tema 3/main.py
@@ -0,0 +1,24 @@
+from def_functions import *
+
+def user_input_f():
+    control_list = ["quit", "exit"]
+
+    while True:
+        user_input = input("Please provide an integer. Or quit/exit to finish.")
+
+        if user_input.isdigit():
+            return {"is_palindrom": palidrom(user_input),
+                    "is_prime": is_prime(user_input),
+                    "divisors": divisor(user_input),
+                    "max_divisor": max_divisor(user_input),
+                    "digits": digits(user_input)
+                    }
+
+        elif user_input.lower() in control_list:
+            print("You finished the program.")
+            break
+
+
+
+
+print(user_input_f())
