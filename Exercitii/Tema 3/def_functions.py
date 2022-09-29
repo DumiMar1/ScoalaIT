2d36349611987bdd7d2c7644c2d4cfe498f8e8be
Dumitru Marin <marindidi96@gmail.com>
2022-09-29 13:35:16 +0300

    Initial commit

--- Exercitii/Tema 3/def_functions.py
+++ Exercitii/Tema 3/def_functions.py
@@ -1,38 +1,59 @@
-def palidrom(num):
+def user_input_f():
 
-    if str(num) == str(num[::-1]):
-        return True
-    else:
-        return False
+    control_list = ["quit", "exit", "0"]
+
+    while True:
+        user_input = input("Please provide an integer(not 0). Or quit/exit to finish.")
+
+        if user_input.lower() in control_list:
+            user_input = None
+            print("You finished the program! ")
+            break
+
+        try:
+            user_input == int(user_input)
+
+        except ValueError:
+
+            user_input = None
+
+        if user_input is not None:
+            break
+    return user_input
 
 
-def is_prime(num):
+def is_palindrome(num):
 
-    num = int(num)
-    if num > 1:
-        for i in range(2, num):
-            if num % i == 0:
-                return False
-        return True
+    return str(num) == str(num[::-1])
 
 
 def divisor(num):
 
     divisors_list = []
-    num = int(num)
+    num = abs(int(num))
     for i in range(2, num):
         if num % i == 0:
             divisors_list.append(i)
     return divisors_list
 
 
-def max_divisor(num):
-    try:
-        return max(divisor(num))
-    except ValueError:
-        return None
-
-
 def digits(num):
-    return len(num)
+    count = 0
+    num = abs(int(num))
+    while num != 0:
+        num //= 10
+        count += 1
+    return count
+
+
+def get_output(num):
+    div = divisor(num)
+    div_len = len(div)
+    num_data = {"is_palindrome": is_palindrome(num),
+                "is_prime": div_len == 0,
+                "divisors": div,
+                "max_divisor": max(div) if div_len > 0 else None,
+                "digits": digits(int(num))
+                }
+    return num_data
 
--- Exercitii/Tema 3/main.py
+++ Exercitii/Tema 3/main.py
@@ -1,24 +1,13 @@
 from def_functions import *
 
+if __name__ == "__main__":
+    user_input = user_input_f()
+    print(user_input)
 
-def user_input_f():
-
-    control_list = ["quit", "exit"]
-
-    while True:
-        user_input = input("Please provide an integer. Or quit/exit to finish.")
-
-        if user_input.isdigit():
-            return {"is_palindrome": palidrom(user_input),
-                    "is_prime": is_prime(user_input),
-                    "divisors": divisor(user_input),
-                    "max_divisor": max_divisor(user_input),
-                    "digits": digits(user_input)
-                    }
-
-        elif user_input.lower() in control_list:
-            print("You finished the program.")
-            break
+    if user_input is not None:
+        number_data = get_output(user_input)
 
 
-print(user_input_f())
+
+
+
